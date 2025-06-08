349dd6f06046642274b4fbc7828eeff4ed91ba71
Elizaveta <emr032005@mail.ru>
2025-06-08 12:40:48 +0300

    Создание проекта

---
+++ SchoolApplication/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace SchoolApplication
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
